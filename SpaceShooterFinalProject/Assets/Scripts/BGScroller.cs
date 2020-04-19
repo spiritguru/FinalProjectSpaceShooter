96d3e263eda5a2638e54f8d9a09097d33c2e7868
spiritguru <59833622+spiritguru@users.noreply.github.com>
2020-04-18 21:02:43 -0400

    FinalProjectSS

---
+++ SpaceShooterFinalProject/Assets/Scripts/BGScroller.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroller : MonoBehaviour
+{
+	public float scrollSpeed;
+	public float tileSizeZ;
+
+	private Vector3 startPosition;
+
+	public GameController gameController;
+	
+
+	void Start()
+	{
+		startPosition = transform.position;
+	}
+
+	void Update()
+	{
+		if (gameController.score >= 100)
+		{
+		
+			scrollSpeed = -50;
+		}
+
+
+		float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeZ);
+		transform.position = startPosition + Vector3.forward * newPosition;
+
+		
+	}
+}
---
+++ SpaceShooterFinalProject/Assets/Scripts/StarField.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarField : MonoBehaviour
+{
+    private ParticleSystem ps;
+    public float StarWin = 1.0F;
+
+    public GameController gameController;
+
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+    }
+
+    void Update()
+    {
+        if (gameController.score>=100)
+        { 
+        var main = ps.main;
+        main.simulationSpeed = StarWin;
+    }
+}
+}
